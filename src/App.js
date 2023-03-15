a6ef3615e2e107db01741ac57e69c2ac6d1ebd34
Shivangi1701 <shivangi170102@gmail.com>
2023-03-15 20:41:20 +0530

    async thunk functionality

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
 import { useSelector, useDispatch } from "react-redux";
-import { calculateTotals } from "./features/cart/cartSlice";
+import { calculateTotals, getCartItems } from "./features/cart/cartSlice";
 import { useEffect } from "react";
 import Modal from "./components/Modal";
 
 function App() {
-  const { cartItems } = useSelector((store) => store.cart);
+  const { cartItems, isLoading } = useSelector((store) => store.cart);
   const { isOpen } = useSelector((store) => store.modal);
   const dispatch = useDispatch();
+
   useEffect(()=>{
     dispatch(calculateTotals());
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [cartItems]);//As cartItems changes effect(calculateTotals) will be re-executed 
+
+  useEffect(()=>{
+    dispatch(getCartItems());
+  }, []);
+
+  if(isLoading){
+    return(
+      <div className="loading">
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
   return (
     <main>
       {isOpen && <Modal />}
--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,5 +1,7 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import cartItems from '../../cartItems';
+
+const url =  'https://course-api.com/react-useReducer-cart-project';
 
 const initialState = {
     cartItems: cartItems,
@@ -7,6 +9,13 @@ const initialState = {
     total: 0,
     isLoading: true,
 };
+
+export const getCartItems = createAsyncThunk(
+    'cart/getCartItems', ()=>{
+        return fetch(url)
+        .then((resp) => resp.json())
+        .catch((err) =>console.log(err));
+});
 
 const cartSlice = createSlice({
     name: 'cart',
@@ -38,6 +47,19 @@ const cartSlice = createSlice({
             });
             state.amount = amount;
             state.total = total;
+        },
+    },
+    extraReducers:{ //lifecycle actions(3) for every function
+        [getCartItems.pending]: (state) => {
+            state.isLoading = true;
+        },
+        [getCartItems.fulfilled]: (state, action) => {
+            console.log(action);
+            state.isLoading = false;
+            state.cartItems = action.payload;
+        },
+        [getCartItems.rejected]: (state) => {
+            state.isLoading = false;
         },
     },
 });
