a0386d3da0996622c65c2e5c9adef061aea1b6fb
Shivangi1701 <shivangi170102@gmail.com>
2023-03-15 12:46:38 +0530

    increase and decrease functionality added

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -18,10 +18,18 @@ const cartSlice = createSlice({
         removeItem: (state, action) => {
             const itemId = action.payload;// contains ID of the item
             state.cartItems = state.cartItems.filter((item) => item.id !== itemId);
-        },//filter method - creates new array of items withour above ID and is returned by reducer as new array
+        },//filter method - creates new array of items without above ID and is returned by reducer as new array
+        increase: (state, {payload}) => {
+            const cartItem = state.cartItems.find((item)=>item.id 
+            === payload.id)
+            cartItem.amount = cartItem.amount + 1;
+        },
+        decrease: (state, {payload}) => {
+            const cartItem = state.cartItems.find((item)=>item.id 
+            === payload.id)
+            cartItem.amount = cartItem.amount - 1;
+        },
     },
 });
-
-//console.log(cartSlice);
-export const { clearCart, removeItem } = cartSlice.actions;
+export const { clearCart, removeItem , increase, decrease } = cartSlice.actions;
 export default cartSlice.reducer;
